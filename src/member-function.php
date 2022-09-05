82331b899a4caf5b4c78dd75bef467ce47ae2aca
tnguyenSwin <111698789+tnguyenSwin@users.noreply.github.com>
2022-09-05 14:01:21 +1000

    Core Member access

---
+++ src/member-function.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.inc'; ?>
+<body>
+    <?php include 'includes/menu.inc'; ?>
+    <h2>List of Member function</h2>
+    <a href="page_member/add-member.php">Add Member Record<br></a>
+    <a href="page_member/validate-memberID-read.php">Read Member Record<br></a>
+    <?php include 'includes/footer.inc'; ?>
+</body>
+</html>
