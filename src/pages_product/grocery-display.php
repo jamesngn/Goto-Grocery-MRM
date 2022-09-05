d6c2fa58485f0bad39a2dd4b5cc1dbdf1911bbbb
jamesngn <jamesnguyennm@gmail.com>
2022-09-05 16:06:30 +1000

    Change folder name

---
+++ src/pages_product/grocery-display.php
@@ -0,0 +1,45 @@
+
+<?php 
+session_start();
+$product = $_SESSION['productAssoc'];
+printf($product);
+include 'includes/header.inc'; 
+?>
+<head>
+    <style>
+        table {
+            width: 70%;
+            margin: 0 auto;
+        }
+        table,th,td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        </style>
+</head>
+<body>
+    <?php include 'includes/menu.inc'; ?>
+    <h2>Read Grocery Item</h2>
+    
+
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Quantity Stock</th>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($product['name']);  ?></td>
+            <td><?php echo htmlspecialchars($product['description']);  ?></td>
+            <td><?php echo htmlspecialchars($product['qty_stock']); ?></td>
+        </tr>
+    </table>
+</body>
+
+<?php 
+session_unset();
+session_destroy();
+include 'includes/footer.inc'; ?>
+    </body>
+</html>
+
---
+++ src/pages_product/grocery-search.php
@@ -0,0 +1,60 @@
+<?php include 'includes/header.inc'; 
+session_start();
+?>
+
+
+<body>
+    <?php include 'includes/menu.inc'; ?>
+    <h2>Validate grocery item ID for reading</h2>
+
+
+    <form action="grocery-search.php" method="post">
+        <fieldset>
+            <legend>Enter the grocery item ID</legend>
+            <p>
+                <label for="id">Grocery ID: </label>
+                <input type="text" name="id" id="id" value >
+            </p>
+            <p>
+                <button type="submit">Search</button>
+                <button type="reset">Reset</button>
+            </p>
+        </fieldset>
+    </form>
+    <?php
+        function cleanInput($data) 
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        include 'includes/dbAuthentication.inc';
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $conn = OpenConnection();
+
+            $c_id = mysqli_real_escape_string($conn,cleanInput($_POST['id']));
+            
+            $sql = "SELECT * FROM product WHERE id = $c_id";
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                if (mysqli_num_rows($result) == 0) {
+                    echo nl2br ("\r\n Error: Product ID is not found.");
+                } else {
+                    $product = mysqli_fetch_assoc($result);
+                    $_SESSION["productAssoc"] = $product;
+                    header("location: grocery-display.php");
+                }
+            } else {
+                echo nl2br ("\r\n SQL error: " . mysqli_error($conn));
+            }
+            CloseConnection($conn);
+        }
+
+    ?>
+
+
+<?php include 'includes/footer.inc'; ?>
+    </body>
+</html>
