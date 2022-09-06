9336d7c559b691949f67276159859e09ec1424f7
tnguyenSwin <111698789+tnguyenSwin@users.noreply.github.com>
2022-09-06 11:04:04 +1000

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -7,12 +7,31 @@
     <a href="pages_member/validate-memberID-edit.php">Update Member Record<br></a>
     <a href="pages_member/validate-memberID-delete.php">Delete Member Record<br></a>
 
+    <h2>List of Sales function</h2>
+
 
     <h2>List of Product functions</h2>
     <a href="pages_product/add-product.php">Add Product Record</a><br>
     <a href="pages_product/validate-productID-read.php">Search Product Record</a><br>
     <a href="pages_product/validate-productID-edit.php">Update Product Record</a><br>
     <a href="pages_product/validate-productID-delete.php">Delete Product Record</a><br>
+
+    <h2>List of Sales function</h2>
+
+
+    <h2>List of SalesDetail function</h2>
+
+
+    <h2>List of Wishlist function</h2>
+
+
+    <h2>List of Supplier function</h2>
+
+
+    <h2>List of Categories functions </h2>
+
+
+    
     <?php include 'includes/footer.inc'; ?>
 </body>
 </html>
