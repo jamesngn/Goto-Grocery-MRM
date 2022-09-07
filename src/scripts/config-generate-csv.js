189e14a5e8f896bda4f98b88d1e8bd4e22732773
tnguyenSwin <111698789+tnguyenSwin@users.noreply.github.com>
2022-09-07 11:29:58 +1000

    Create config-generate-csv.js
    Checkbox require for group

---
+++ src/scripts/config-generate-csv.js
@@ -0,0 +1,25 @@
+/**
+* Author: Thanh 101607169
+* Target: configuration-generate-csv.php
+* Purpose: Validation/required
+* Created: 07/09/2022
+* Last Updated: 07/09/2022
+* Credits: (Any guidance/help/code? Credit it here.)
+*/
+
+  function handleData()
+{
+    var form_data = new FormData(document.querySelector("form"));
+    
+    if(!form_data.has("Table[]"))
+    {
+        document.getElementById("chk_option_error").style.visibility = "visible";
+      return false;
+    }
+    else
+    {
+        document.getElementById("chk_option_error").style.visibility = "hidden";
+      return true;
+    }
+    
+}
