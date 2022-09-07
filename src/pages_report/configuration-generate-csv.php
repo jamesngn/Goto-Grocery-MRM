2611a29a73b8d4d99a841d354128aad98775410f
tnguyenSwin <111698789+tnguyenSwin@users.noreply.github.com>
2022-09-07 11:30:17 +1000

    Update configuration-generate-csv.php
    Adding js scription

--- src/pages_report/configuration-generate-csv.php
+++ src/pages_report/configuration-generate-csv.php
@@ -1,3 +1,4 @@
+<script src="../scripts/config-generate-csv.js"></script>
 <?php include '../includes/header.inc'; 
 ?>
 <body>
@@ -6,16 +7,19 @@
     <form method="post" action="generate-csv.php">
   		<!--Table Allocation-->
 	<fieldset id="csv_table_name">
-		<legend> csv_table_name</legend>
+		<legend> Select all the scripting languages you know:</legend>
+        <div style="visibility:hidden; color:red; " id="chk_option_error">
+        Please select at least one option.
+        </div>
 			<p id="checkbox">	
                 <label for="member">Member Data</label> 
-				<input type="checkbox" id="member" name="Table[]" value="member"/>
+				<input type="checkbox" id="member_mysql_table" name="Table[]" value="member"/>
 
 				<label for="product">Product Data</label> 
-				<input type="checkbox" id="product" name="Table[]" value="product"/>
+				<input type="checkbox" id="product_mysql_table" name="Table[]" value="product"/>
 
                 <label for="category">Catergory Data</label> 
-				<input type="checkbox" id="category" name="Table[]" value="category"/>
+				<input type="checkbox" id="category_mysql_table" name="Table[]" value="category"/>
 			</p>
             <p>
             <input type="submit" value="Submit">
