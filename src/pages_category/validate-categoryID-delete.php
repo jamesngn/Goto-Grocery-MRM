9d49578e49c3f5fa1685a0e29c5f28932f57e4c7
Jespa01 <100499110+Jespa01@users.noreply.github.com>
2022-09-07 18:52:21 +1000

    Debugged Delete of categories

--- src/pages_category/validate-categoryID-delete.php
+++ src/pages_category/validate-categoryID-delete.php
@@ -7,8 +7,8 @@
     <h2>Validate category ID for deleting</h2>
     <form action="validate-categoryID-delete.php" method="post">
         <p>
-            <label for="id">Category ID:</label>
-            <input type="text" name="id" id="id">
+            <label for="categoryID">Category ID:</label>
+            <input type="text" name="categoryID" id="categoryID">
         </p>
         <button type="submit">Delete</button>
     </form>
@@ -25,9 +25,9 @@
 
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $conn = OpenConnection();
-            $category_id = mysqli_real_escape_string($conn,cleanInput($_POST['id']));
+            $category_id = mysqli_real_escape_string($conn,cleanInput($_POST['categoryID']));
 
-            $sql = "SELECT * FROM category WHERE id = '$category_id'";
+            $sql = "SELECT * FROM category WHERE categoryID = '$category_id'";
             $result = mysqli_query($conn,$sql);
             if ($result) {
                 if (mysqli_num_rows($result) == 0) {
