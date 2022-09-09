d6252e171fc3728f3fbb8bab146ae278408ade1e
golam-tahmid <golamtahmid14@gmail.com>
2022-09-09 16:11:12 +1000

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -28,7 +28,7 @@
 
 
     <h2>List of Wishlist function</h2>
-    <a href="wishlist/add_new_wishlist.php">Add Supplier Record</a><br>
+    <a href="wishlist/add_new_wishlist.php">Add Wishlist Record</a><br>
 
     <h2>List of Supplier function</h2>
     <a href="pages_supplier/add-supplier.php">Add Supplier Record</a><br>
