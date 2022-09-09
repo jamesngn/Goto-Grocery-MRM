88bacffa683487cc7f12c5a2aeac74e7028ca881
Jespa01 <100499110+Jespa01@users.noreply.github.com>
2022-09-09 19:15:01 +1000

    Validated read wishlist

--- src/pages_wishlist/read-wishlist.php
+++ src/pages_wishlist/read-wishlist.php
@@ -1,70 +1,46 @@
 <?php include '../includes/header.inc';
 session_start();
+$regValue = $_SESSION["wishlistid"];
 ?>
 <body>
     <?php include '../includes/menu.inc'; ?>
     <h2>Read Wishlist</h2>
-
-    <form method="post" action="read_new_wishlist.php">
-        <fieldset>
-            <legend>Enter new wishlist details</legend>
-            <p>
-                <label for="wishlistid">Enter Wishlist ID</label>
-                <input type="text" name="wishlistid" id="wishlistid" pattern="\d{1,10}" maxlength="10" required />
-            </p>
-            <p>
-            <input type="submit" value="Submit">
-            <input type="reset"> 
-            </p>
-        </fieldset>
-    </form>
-
+    
     <?php
-        function cleanInput($data) 
-        {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-        
-        include '../includes/dbAuthentication.inc';
-        
-       if ($_SERVER["REQUEST_METHOD"] == "POST")
-        {
-            $conn = OpenConnection();
-
-         
-          
-            $result =mysqli_query($conn, $sql);
-            $sql = "SELECT * FROM wishlist WHERE wishlistid = '$wishid' ";
+    echo nl2br("\r\n". $_SESSION["wishlistid"]);
+    $c_wishlistID = $_SESSION["wishlistid"];
+    include '../includes/dbAuthentication.inc';
+    $conn = OpenConnection();
+    $sql = "SELECT * FROM wishlist WHERE wishlistid = '$c_wishlistID' ";
             if(mysqli_query($conn, $sql))
             {
                 $result=mysqli_query($conn, $sql);
-                if (mysqli_num_rows($result)==0)
+                if (mysqli_num_rows($result)>0)
                 {
-                    echo nl2br("\r\n Error:Check ID is correct.");
-
+                    while($row = $result->fetch_assoc())
+                    {
+                    echo nl2br("\r\n Wishlist ID: " . $row["wishlistid"]);
+                    echo nl2br("\r\n Customer ID: ". $row["cust_id"]);
+                    echo nl2br("\r\n Product ID: ". $row["prod_id"]);  
+                    
+                    }
+                    session_unset();
+                    session_destroy();
                 }
                 else{
-                 
-                    $_SESSION["wishlistid"] =$wishid;
-                    header ("location: read_new_wishlist.php");
+              
+                    echo nl2br("\r\n Error:DB is incorrect.");
             
-                }
-                
+                }              
             }
             else
             {
                 echo nl2br("\r\n SQL error: " . mysqli_error($conn));
             }
-            CloseConnection($conn);
-          //  $conn->close();
-        }
+    CloseConnection($conn);
     ?>
 
     <?php include '../includes/footer.inc'; ?>
-    <?php include '../includes/bootstrapcore.inc'; ?>
 </body>
 
 </html>
