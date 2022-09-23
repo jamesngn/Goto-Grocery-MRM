fe11409f47f4e658ef914cf361d1fe1b006b4ac4
Jespa01 <100499110+Jespa01@users.noreply.github.com>
2022-09-23 16:33:16 +1000

    debugged log in

--- src/pages_customer_login_logout/customer_login.php
+++ src/pages_customer_login_logout/customer_login.php
@@ -11,7 +11,7 @@ session_start();//session starts here
 	<form method="post" action="">
 	<fieldset><legend>Customer Log-in</legend>
 		<p>	<label for="email">Email: </label>
-			<input type="text" name="email" id="email"  /></p>
+			<input type="text" name="email" id="email"  pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" maxlength="50" required /></p>
 		<p>	<label for="password">Password: </label>
 			<input type="text" name="password" id="password"  /></p>
 		<p>	<input type="submit" value="Log In" /></p>
@@ -44,6 +44,7 @@ session_start();//session starts here
 			$err_msg .= "<p>Only letters and number are allowed in password.</p>";
 	}
 	}
+	
 	// email
 	if (trim($email) == "")
 		$err_msg .= "<p>Please enter email.</p>";
@@ -52,14 +53,15 @@ session_start();//session starts here
 		$email =  sanitise_input($email);
 	}
 	
+	
 	if ($err_msg!=""){
 		echo $err_msg;
 	    exit();
 	}
+
 	
-
-	if ((!$password=="" && $email=="")){
-
+	if ((!$password=="" && !$email=="")){
+		
 	include '../includes/dbAuthentication.inc';
         
     
@@ -72,7 +74,7 @@ session_start();//session starts here
 		echo "<p >Database connection failure</p>";  
 	} else {
 		// Upon successful connection
-		echo nl2br("\r\n SQL error:1 ");
+		
 	$sql_table="member";
 	
 		// Setting up the SQL command to add the data into the table
@@ -96,7 +98,7 @@ session_start();//session starts here
 		} 
 		// close the database connection
 		mysqli_close($conn);
-	} 
+	} //echo nl2br("\r\n SQL error:1 "); // for debugging
 	} 
 ?>
 
