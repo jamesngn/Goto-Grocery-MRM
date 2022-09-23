d96c4aa064f8aeb6914a599a7e8fedd54045e687
golam-tahmid <golamtahmid14@gmail.com>
2022-09-23 14:42:47 +1000

    yo
    yo

--- src/pages_supplydelivery/delete-supply-delivery.php
+++ src/pages_supplydelivery/delete-supply-delivery.php
@@ -1,21 +1,22 @@
 <?php 
     session_start();
-    $supp_id = $_SESSION['supplydeliveryid'];
+    $suppdel_id = $_SESSION['supplydeliveryid'];
     include '../includes/header.inc' 
 ?>
 <body>
     <?php include '../includes/menu.inc' ?>;
-    <h2>Delete Supplier</h2>
+    <h2>Delete Supply Delivery</h2>
 
     
     <?php 
         include '../includes/dbAuthentication.inc';
         $conn = OpenConnection();
 
-        $sql = "DELETE FROM suppdelivery WHERE supplydeliveryid = '$supp_id'";
+        $sql = "DELETE FROM suppdelivery 
+        WHERE supplydeliveryid = '$suppdel_id'";
 
         if (mysqli_query($conn, $sql)) {
-            echo nl2br("\r\n Successfully deleted the supplier_id = $supp_id from the database");
+            echo nl2br("\r\n Successfully deleted the supply delivery ID = $suppdel_id from the database");
             session_unset();
             session_destroy();
         } else {
--- src/pages_supplydelivery/validate-supplydelivery-delete.php
+++ src/pages_supplydelivery/validate-supplydelivery-delete.php
@@ -4,11 +4,11 @@
 ?>
 <body>
     <?php include '../includes/menu.inc'; ?>
-    <h2>Delete supply delivery</h2>
+    <h2>Validate Supply Delivery for deleting</h2>
     <form action="validate-supplydelivery-delete.php" method="post">
         <p>
-                <label for="supplydeliveryid">Suppydelivery ID</label>
-                <input type="text" name="supplydeliveryid" id="supplydeliveryid">
+            <label for="supplydeliveryid">Supply Delivery ID:</label>
+            <input type="text" name="supplydeliveryid" id="supplydeliveryid">
         </p>
         <button type="submit">Delete</button>
     </form>
@@ -25,21 +25,21 @@
 
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $conn = OpenConnection();
-            $supp_id = mysqli_real_escape_string($conn,cleanInput($_POST['supplydeliveryid']));
+            $suppdel_id = mysqli_real_escape_string($conn,cleanInput($_POST['supplydeliveryid']));
 
-            $sql = "SELECT * FROM suppdelivery WHERE supplydeliveryid = '$supp_id'";
+            $sql = "SELECT * FROM suppdelivery WHERE supplydeliveryid = '$suppdel_id'";
             $result = mysqli_query($conn,$sql);
             if ($result) {
                 if (mysqli_num_rows($result) == 0) {
-                    echo nl2br("\r\n Error: Supply delivery is not found.");
+                    echo nl2br("\r\n Error: Supply Delivery ID $suppdel_id is not found.");
                 } else {
-                    $_SESSION['supplydeliveryid'] = $supp_id;
-                    header("location: validate-supplydelivery-delete.php");
+                    $_SESSION['supplydeliveryid'] = $suppdel_id;
+                    header("location: delete-supply-delivery.php");
                 }
             } else {
                 echo nl2br("\r\n SQL Error: " . mysqli_error($conn));
             }
-            CloseConnection($conn);
         }
+        CloseConnection($conn);
     ?>
 </body>
