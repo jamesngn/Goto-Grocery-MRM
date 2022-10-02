7e12752299a8be3483ff59df9c2b782e340c9dd0
jamesngn <jamesngn@gmail.com>
2022-10-03 01:02:18 +1100

    add product success  response page

---
+++ frontBackCombined/pages/add-product-success.php
@@ -0,0 +1,35 @@
+<?php 
+    include '../includes/header.inc';
+?>
+<body>
+    <?php include '../includes/sidebar.inc';?>;
+    <section class="home-section">
+        <div class="top-bar">
+            <i class="fas fa-solid fa-bars"></i>
+            <span class="title">ADD NEW PRODUCT</span>
+        </div>
+        <div class="reponse-container">
+            <div class="info">
+                <img class="success-icon" src="../image/success-icon.png" alt="success-check-icon">
+                <div class="message-container">
+                    <div class="big-message">
+                        Addition Success
+                    </div>
+                    <div class="small-message">
+                        Wait a few minutes while the information is validated
+                    </div>
+                </div>
+            </div>
+            <button id="addMoreButton" class="add-more" onclick="RedirectToAddProductPage()">
+                Add More
+            </button>
+            <button id = "closeButton" class="close" onclick="RedirectToProductPage()">
+                Close
+            </button>
+        </div>
+
+    </section>
+
+<script src="../js/product.js"></script>        
+</body>
+</html>
