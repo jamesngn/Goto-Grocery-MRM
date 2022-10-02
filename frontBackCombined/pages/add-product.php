c9485065d8e06b71a69f4370e68b1c32f446af33
jamesngn <jamesngn@gmail.com>
2022-10-02 21:35:01 +1100

    process category when adding product

--- frontBackCombined/pages/add-product.php
+++ frontBackCombined/pages/add-product.php
@@ -3,7 +3,7 @@
 ?>
 <body>
     <?php include '../includes/sidebar.inc';?>;
-    
+
     <section class="home-section">
         <div class="top-bar">
             <i class="fas fa-solid fa-bars"></i>
@@ -43,12 +43,30 @@
                             <label for="price">Price</label>
                             <input type="number" name="price" id="price">
                         </div>
+
+                        <?php 
+                            include '../includes/dbAuthentication.inc';
+
+                            $conn = OpenConnection();
+
+                            $sql = "SELECT * FROM category";
+                            $result = mysqli_query($conn,$sql);
+
+                            if ($result) {
+                                if (mysqli_num_rows($result) > 0) {
+                                    $categories = mysqli_fetch_all($result);
+                                }
+                            } else {
+                                echo nl2br ("\r\n SQL error: " . mysqli_error($conn));
+                            }
+                        ?>
                         <div class="form-item">
                             <label for="category">Category</label>
                             <select name="category" id="category">
                                 <option value=""></option>
-                                <option value="phone">Phone</option>
-                                <option value="laptop">Laptop</option>
+                                <?php foreach($categories as $category): ?>
+                                    <option value="<?php echo $category[0];?>"><?php echo $category[1];?></option>
+                                <?php endforeach; ?>
                             </select>
                         </div>
                     </div>
@@ -76,38 +94,79 @@
 </body>
 </html>
 
-<?php
 
-if(isset($_POST["submit"])) {
-  $file = $_FILES['productImgToUpload'];
+<?php 
 
-  $fileName = $_FILES['productImgToUpload']['name'];
-  $fileTmpName = $_FILES['productImgToUpload']['tmp_name'];
-  $fileSize = $_FILES['productImgToUpload']['size'];
-  $fileError = $_FILES['productImgToUpload']['error'];
-  $fileType = $_FILES['productImgToUpload']['type'];
-
-  $fileExt = explode('.',$fileName);
-  $fileActualExt = strtolower(end($fileExt));
-
-  $allowed = array('jpg','jpeg','png');
-
-  if (in_array($fileActualExt,$allowed)) {
-    if ($fileError === 0) {
-        if ($fileSize < 500000) {
-            $fileNameNew = uniqid('',true).".".$fileActualExt;
-            $fileDestination = '../image/'.$fileNameNew;
-            move_uploaded_file($fileTmpName,$fileDestination);
-            echo "Successfully uploaded the image!";
-        } else {
-            echo "Your file is too big!";
-        }
-    } else {
-        echo "There was an error uploading your file!";
+    function cleanInput($data) 
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-  } else {
-    echo "You cannot upload files of this type!";
-  }
-}
 
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        
+        //select the latest id
+        $sql = "SELECT id FROM product ORDER BY id DESC LIMIT 1";
+        $result = mysqli_query($conn,$sql);
+
+        if ($result) {
+            if (mysqli_num_rows($result) > 0) {
+                $productID = mysqli_fetch_row($result);
+            }
+        } else {
+            echo nl2br ("\r\n SQL error: " . mysqli_error($conn));
+        }
+
+        // the cleaned – "safe" – inputs ready to be added to the database
+        $c_grocery_name = mysqli_real_escape_string($conn, cleanInput($_POST['displayName']));
+        $c_description = mysqli_real_escape_string($conn, cleanInput($_POST['description']));
+        $c_price = mysqli_real_escape_string($conn, cleanInput($_POST['price']));
+        $c_category_id = mysqli_real_escape_string($conn, cleanInput($_POST['category']));
+        
+        //process the image
+        $file = $_FILES['productImgToUpload'];
+
+        $fileName = $_FILES['productImgToUpload']['name'];
+        $fileTmpName = $_FILES['productImgToUpload']['tmp_name'];
+        $fileSize = $_FILES['productImgToUpload']['size'];
+        $fileError = $_FILES['productImgToUpload']['error'];
+        $fileType = $_FILES['productImgToUpload']['type'];
+
+        $fileExt = explode('.',$fileName);
+        $fileActualExt = strtolower(end($fileExt));
+
+        $allowed = array('jpg','jpeg','png');
+
+        if (in_array($fileActualExt,$allowed)) {
+            if ($fileError === 0) {
+                if ($fileSize < 500000) {
+                    $fileNameNew = $productID.".".$fileActualExt;
+                    $fileDestination = '../image/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName,$fileDestination);
+                    echo "Successfully uploaded the image!";
+                } else {
+                    echo "Your file is too big!";
+                }
+            } else {
+                echo "There was an error uploading your file!";
+            }
+        } else {
+            echo "You cannot upload files of this type!";
+        }
+
+        //Add to database
+        $sql = "INSERT INTO product (name,description,retailPrice,category_ID)
+        VALUES ('$c_grocery_name','$c_description','$c_price','$c_category_id')";
+
+        if (mysqli_query($conn,$sql)) {
+            
+        } else {
+            echo nl2br ("\r\nSQL errror: " . mysqli_error($conn));
+        }
+        CloseConnection($conn);
+    }
 ?>
