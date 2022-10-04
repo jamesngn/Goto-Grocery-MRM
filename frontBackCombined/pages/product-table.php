fdcb3fc3f8efe5b113960b4d79a0c38107aef751
jamesngn <103204978+jamesngn@users.noreply.github.com>
2022-10-05 01:03:38 +1100

    add the table element

--- frontBackCombined/pages/product-table.php
+++ frontBackCombined/pages/product-table.php
@@ -24,11 +24,72 @@
             </div>
         </div>
 
+        <div class="table-container">
+            <table>
+                <thead>
+                        <th class="checkBox">
+                            <input type="checkbox" name="all" id="">
+                        </th>
+                        <th class="imageHeading">Image</th>
+                        <th>ID</th>
+                        <th class="nameHeading">Name</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th class="actionHeading">Actions</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="checkBox"><input type="checkbox" name="100000" id="10000" onclick="highlightProduct(this)"></td>
+                        <td><img src="../image/product/100000.png" alt=""></td>
+                        <td>100000</td>
+                        <td>iPhone 14 Pro Max</td>
+                        <td>$2000.00</td>
+                        <td>Phone</td>
+                        <td class="actions">
+                            <form action="read-product.php" method="get">
+                                <input type="hidden" name="productID" value="100000">
+                                <button type="submit"><i class="fa-solid fa-eye"></i></button>
+                            </form>
+                            <form action="edit-product.php" method="get">
+                                <input type="hidden" name="productID" value="100000">
+                                <button type="submit"><i class="fa-solid fa-pen"></i></button>
+                            </form>
+                            <i class="fa-solid fa-trash" onclick="displayDeleteMessage()"></i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="checkBox"><input type="checkbox" name="100000" id="10000" onclick="highlightProduct(this)"></td>
+                        <td><img src="../image/product/100000.png" alt=""></td>
+                        <td>100000</td>
+                        <td>iPhone 14 Pro Max</td>
+                        <td>$2000.00</td>
+                        <td>Phone</td>
+                        <td class="actions delete-message">
+                            <div class="question">
+                                DELETE ?
+                            </div>
+                            <div class="choice">
+                                <div class="yes-choice">
+                                    <form action="delete-product.php" method="get">
+                                        <input type="hidden" name="productID" value="100000">
+                                        <button type="submit">
+                                            <i class="fa-solid fa-check"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                                <div class="no-choice">
+                                    <i class="fa-solid fa-xmark"></i>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    
+         
+                    
+                </tbody>
+            </table>
+        </div>
 
-        <form action="read-product.php" method="get">
-            <input type="hidden" name="productID" value="100000">
-            <button type="submit">Read Product</button>
-        </form>
 
 
     </section>
