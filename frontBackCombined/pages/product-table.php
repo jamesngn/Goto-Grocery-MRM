102c3d9ced56906db2bd308722581a7677c64e0d
jamesngn <103204978+jamesngn@users.noreply.github.com>
2022-10-04 15:09:40 +1100

    add elements for product table page

--- frontBackCombined/pages/product-table.php
+++ frontBackCombined/pages/product-table.php
@@ -3,11 +3,27 @@
 ?>
 <body>
     <?php include '../includes/sidebar.inc';?>;
-    <section class="home-section">
+    <section class="home-section tablePage">
         <div class="top-bar">
             <i class="fas fa-solid fa-bars"></i>
             <span class="title">PRODUCT TABLE</span>
         </div>
+
+        <div class="top-section">
+            <div class="left-items">
+                <div class="pageTitle">Product</div>
+                <div class="search-bar">
+                    <form action="product-table.php" method="get">
+                        <input type="text" name="query" id="query" placeholder="Search Products">
+                        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </form>
+                </div>
+            </div>
+            <div class="right-items addProductButton">
+                <a href="add-product.php">+ Add product</a>
+            </div>
+        </div>
+
 
         <form action="read-product.php" method="get">
             <input type="hidden" name="productID" value="100000">
