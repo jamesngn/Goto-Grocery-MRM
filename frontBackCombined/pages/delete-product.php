b439e1bb36c3eab739503e03ff780d0cf9b6e27f
jamesngn <103204978+jamesngn@users.noreply.github.com>
2022-10-05 11:07:28 +1100

    add function to delete image in the folder

--- frontBackCombined/pages/delete-product.php
+++ frontBackCombined/pages/delete-product.php
@@ -4,8 +4,19 @@
 
     $id = $_POST['delete_id'];
 
+    
+    $sql = "SELECT image FROM product WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            $product = mysqli_fetch_assoc($result);
+            echo $product['image'];
+            unlink($product['image']);
+        }
+    }
         
-    $sql = "DELETE FROM product WHERE id = '$id'";
+    $sql = "DELETE FROM product WHERE id = $id";
     $result = mysqli_query($conn,$sql);
 
+    CloseConnection($conn);
 ?>
