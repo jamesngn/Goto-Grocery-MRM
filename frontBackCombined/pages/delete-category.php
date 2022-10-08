f2e7033f6d008b26c16416534093189555f54e9e
Jespa01 <100499110+Jespa01@users.noreply.github.com>
2022-10-08 13:23:51 +1100

    delete

---
+++ frontBackCombined/pages/delete-category.php
@@ -0,0 +1,12 @@
+<?php
+    include '../includes/dbAuthentication.inc';
+    $conn = OpenConnection();
+
+    $id = $_POST['delete_id'];
+
+        
+    $sql = "DELETE FROM product WHERE id = $id";
+    $result = mysqli_query($conn,$sql);
+
+    CloseConnection($conn);
+?>
