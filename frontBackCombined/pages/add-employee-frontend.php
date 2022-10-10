7e0bedf09a7e7b65ee3e7e47a5de30df6e795e55
Sajid Muntasir <muntasir.sajid39@gmail.com>
2022-10-10 23:58:56 +1100

    Front end Employee

---
+++ frontBackCombined/pages/add-employee-frontend.php
@@ -0,0 +1,64 @@
+<?php
+include '../includes/header.inc';
+?>
+
+<body>
+    <?php include '../includes/sidebar.inc'; ?>;
+    <section class="home-section">
+        <div class="top-bar">
+            <i class="fas fa-solid fa-bars"></i>
+            <span class="title">Add NEW EMPLOYEE</span>
+        </div>
+
+        <div class="form-container">
+            <div id="addProductForm">
+                <form action="add-employee-validation.php" method="post">
+
+                    <div class="backButton">
+                        <a href="employee.php">
+                            <i class="fa-solid fa-delete-left"></i>
+                            <span>EMPLOYEE Page</span>
+                        </a>
+                    </div>
+                    <div class="text-input-container">
+                        <div class="form-wrap">
+                            <div class="form-item">
+                                <label for="fname">First name</label>
+                                <input type="text" name="fname" id="fname" pattern="^[A-Za-z-]+$" maxlength="50" required />
+                            </div>
+                        </div>
+                        <div class="form-wrap">
+                            <div class="form-item">
+                                <label for="lname">Last name</label>
+                                <input type="text" name="lname" id="lname" pattern="^[A-Za-z-]+$" maxlength="50" required />
+                            </div>
+                        </div>
+                        <div class="form-wrap">
+                            <div class="form-item">
+                                <label for="email">Email</label>
+                                <input type="text" name="email" id="email" pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" maxlength="50" required />
+                            </div>
+                        </div>
+                        <div class="form-wrap">
+                            <div class="form-item">
+                                <label for="password">Password</label>
+                                <input type="text" name="password" id="password" maxlength="50" required />
+                            </div>
+                        </div>
+                        <div class="form-wrap">
+                            <button class="Add" type="submit" name="submit">Add</button>
+                            <button type="reset" onclick="ResetInput()">Reset</button>
+                        </div>
+
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </section>
+    <script src="../js/member.js"></script>
+    <script src="../js/sidebar.js"></script>
+</body>
+
+</html>
